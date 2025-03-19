831d2c2bb727aa13513ea548fa8dae38c72c0529
Joel <joelrzief@gmail.com>
2025-03-19 18:27:21 -0400

    db model completed

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,12 @@
+//import { singlestoreTable } from 'drizzle-orm/singlestore-core';
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, bigint, singlestoreTable, index, singlestoreTableCreator} from "drizzle-orm/singlestore-core";
 
 
 
-export const users = singlestoreTable("users_table", {
+/* export const users = singlestoreTable("users_table", {
 
   id: int("id").primaryKey().autoincrement(),
 
@@ -13,7 +14,33 @@ export const users = singlestoreTable("users_table", {
 
   age: int("age"),
 
-});
+}); */
+
+export const createTable = singlestoreTableCreator(
+  (name) => `theo-drive-tutorial_${name}`,
+);
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+},
+ (t) => {
+  return [index("parent_index").on(t.parent)];
+ },
+);
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+},
+ (t) => {
+  return [index("parent_index").on(t.parent)];
+ },
+);
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
