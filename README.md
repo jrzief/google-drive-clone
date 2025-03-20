95d79add72461ffdafef2662da6f1dcfe474de5f
Joel <joelrzief@gmail.com>
2025-03-19 22:06:05 -0400

    Folder Ids now handled by routes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## TODO
 
-- [ ] Set up Database and data model
+- [x ] Set up Database and data model
 - [ ] Move folder open state to URL
 - [ ] Add auth
 - [ ] Add file uploading
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 import { db } from "~/server/db";
+
 import { files as filesSchema, folders as foldersSchema} from "~/server/db/schema";
 import DriveContents from "./drive-contents";
 //import Link from "next/lin
 
 
-export default async function GoogleDriveClone() {
-  const files = await db.select().from (filesSchema);
-  const folders = await db.select().from (foldersSchema);
-  return <DriveContents files={files} folders={folders} />
+export default function HomePage() {
+  //const files = await db.select().from (filesSchema);
+  //const folders = await db.select().from (foldersSchema);
+  //return <DriveContents files={files} folders={folders} />
+  return <div>{"Please subscribe you're two hours in you nerd"}</div>;
 }
