541e39d42d014fd67623109639ba65ea381b9db1
Joel <joelrzief@gmail.com>
2025-03-25 23:15:50 -0400

    Add all files

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -142,7 +142,7 @@ export default function DriveContents(props: {
           </ul>
         </div>
         <UploadButton
-          endpoint="imageUploader"
+          endpoint="driveUploader"
           onClientUploadComplete={() => {
             navigate.refresh();
           }}
