c96da55be6277933a5661d1744f57941655569e9
Joel <joelrzief@gmail.com>
2025-03-30 15:35:50 -0400

    signin flow, start of inboarding

--- src/app/(home)/sign-in/page.tsx
+++ src/app/(home)/sign-in/page.tsx
@@ -7,7 +7,7 @@ export default function Signin() {
   return (
    <> 
      <main className="text-center">
-      <SignInButton />
+      <SignInButton forceRedirectUrl={"/drive"} />
      </main>  
      <footer className="mt-16 text-sm text-neutral-500">
        © {new Date().getFullYear()} T3 Drive. All rights reserved.
